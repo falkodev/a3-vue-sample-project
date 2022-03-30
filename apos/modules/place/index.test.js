a549b64aba9148ab15a9621e6f6d94879c2d6cc0
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-03-31 01:13:46 +0200

    Google Maps API for Places

--- apos/modules/place/index.test.js
+++ apos/modules/place/index.test.js
@@ -17,20 +17,26 @@ describe('place', () => {
           results: [
             {
               name: 'Wine store',
-              location: {
-                formatted_address:
-                  '12 avenue Charles de Gaulle, 34001 Montpellier',
-              },
-              geocodes: {
-                main: {
-                  longitude: 3.543093,
-                  latitude: 43.652038,
+              vicinity: '12 avenue Charles de Gaulle, 34001 Montpellier',
+              geometry: {
+                location: {
+                  lgn: 3.543093,
+                  lat: 43.652038,
                 },
               },
+              photos: [
+                {
+                  photo_reference:
+                    'Aap_uED7w4Zs0q4lAy3YPJ__J4FE1-Ov5rNOP8A3bdGa9p_yKXCYuFcJDYddEbC4Sj6nd_P1dlSzSOWcJ7pKe6Z8s53FmAv5CG-l5scTUJyZR6BpvRBbsNZlbc5IGx5jKbR18VN7DHxeAp34FEruTOJu3v9ZMtW55NLqI7ngxmbAviNnGwev',
+                },
+              ],
             },
           ],
         }
       }),
+    },
+    attachment: {
+      insert: jest.fn(),
     },
   }
 
@@ -62,12 +68,16 @@ describe('place', () => {
   test('fixtures are run', async () => {
     const runFixtures = place.handlers(self)['@apostrophecms/db:fixtures']
     await runFixtures.placeFixtures({})
-    const { url, querystring, headers } = config.get('placesAPI')
-    const urlQuery = qs.stringify(querystring)
-    const finalUrl = `${url}?${urlQuery}`
+    const { url } = config.get('placesAPI')
+    const searchUrlQuery = qs.stringify(url.search.querystring)
+    const searchUrl = `${url.prefix}/${url.search.url}?${searchUrlQuery}`
     expect(self.apos.util.log).toHaveBeenCalled()
-    expect(self.apos.http.get).toHaveBeenCalledWith(finalUrl, { headers })
+    expect(self.apos.http.get).toHaveBeenCalledWith(searchUrl)
+    expect(self.apos.attachment.insert).toHaveBeenCalled()
     expect(self.insert).toHaveBeenCalled()
+
+    self.apos.attachment = {}
+    await runFixtures.placeFixtures({})
 
     self.apos.http = {}
     await runFixtures.placeFixtures({})
