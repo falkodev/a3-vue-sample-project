42cda2ec84685d49d568ff1e32572d680c7e09d7
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-03-31 19:53:11 +0200

    Tweak docker-compose DB

---
+++ apos/modules/@apostrophecms/ui/ui/apos/components/AposCellBasic.vue
@@ -0,0 +1,21 @@
+<script>
+import AposCellBasic from 'apostrophe/modules/@apostrophecms/ui/ui/apos/components/AposCellBasic.vue'
+
+export default {
+  extends: AposCellBasic,
+  methods: {
+    get(fieldName) {
+      const result = AposCellBasic.mixins[0].methods.get.call(this, fieldName)
+      const translation = this.$t('apostrophe:' + result)
+      if (!translation.includes('apostrophe:')) {
+        // translation succeeded
+        return translation
+      }
+
+      // 'apostrophe:' still in translation meaning it wasn't a field to translate,
+      // so return original result
+      return result
+    },
+  },
+}
+</script>
