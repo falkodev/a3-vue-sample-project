de2ecd1e17125860a40d20d2aa8fb51ed3be8f55
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-05-18 11:32:11 +0200

    Fixed components

--- vue/src/components/VisitItem.vue
+++ vue/src/components/VisitItem.vue
@@ -7,7 +7,7 @@
     <div
       class="t-visit__infos t-infos"
       :class="{
-        't-visit__infos-domain': this.step._place[0].placeType == 'domain',
+        't-visit__infos--domain': this.step._place[0].placeType == 'domain',
       }"
     >
       <div class="t-infos__title">{{ this.step._place[0].title }}</div>
@@ -38,7 +38,7 @@
           {{ this.step._place[0].placeType }}
         </div>
         <div class="t-infos__visit">
-          ・ {{ this.translation.SelfGuidedTour }}
+          ・ {{ this.translation.selfGuidedTour }}
         </div>
       </div>
       <div class="t-infos__footer">
@@ -51,21 +51,8 @@
           </div>
         </div>
         <div class="t-infos__fav">
-          <div class="t-infos__heart-container">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="t-infos__heart"
-              viewBox="0 0 20 20"
-              fill="currendColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="">Favoris</div>
+          <HeartIcon />
+          <div class="">{{ this.translation.favorite }}</div>
         </div>
       </div>
     </div>
@@ -105,6 +92,7 @@
 </template>
 
 <script>
+import HeartIcon from './HeartIcon.vue'
 export default {
   name: 'VisitItem',
   props: {
@@ -114,107 +102,100 @@ export default {
     translation: Object,
   },
   emits: ['delItem'],
+  components: {
+    HeartIcon,
+  },
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .t-visit {
   margin: 16px 0;
   display: flex;
   flex-direction: row;
   align-items: center;
   width: 100%;
-}
 
-.t-visit__image {
-  display: flex;
-  flex-direction: row;
-  width: 20%;
-  border-radius: 15px;
-}
-.t-visit__infos {
-  padding: 10px 16px;
-  display: flex;
-  flex-direction: column;
-  width: 70%;
-  border-radius: 15px;
-  border: 1px solid #5357c145;
-  background-color: white;
-}
-.t-visit__infos-domain {
-  padding: 10px 16px;
-  display: flex;
-  flex-direction: column;
-  width: 70%;
-  border-radius: 15px;
-  border: 1px solid #5357c145;
-  background-color: #5357c145;
-}
+  &__image {
+    display: flex;
+    flex-direction: row;
+    width: 20%;
+    border-radius: 15px;
+  }
 
-.t-visit__description {
-  display: flex;
-  flex-direction: column;
-}
+  &__infos {
+    padding: 10px 16px;
+    display: flex;
+    flex-direction: column;
+    width: 70%;
+    border-radius: 15px;
+    border: 1px solid #5357c145;
+    background-color: white;
 
-.t-visit__bin {
-  width: 10%;
-  fill: #5357c1;
-  color: #5357c1;
-}
+    &--domain {
+      padding: 10px 16px;
+      display: flex;
+      flex-direction: column;
+      width: 70%;
+      border-radius: 15px;
+      border: 1px solid #5357c145;
+      background-color: #5357c145;
+    }
+  }
 
+  &__description {
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__bin {
+    width: 10%;
+    fill: #5357c1;
+    color: #5357c1;
+  }
+}
 .t-infos {
   color: #5357c1;
-}
-.t-infos__title {
-  font-weight: bold;
-}
 
-.t-infos__type {
-  font-size: 15px;
-}
+  &__title {
+    font-weight: bold;
+  }
 
-.t-infos__visit {
-  font-size: 12px;
-}
+  &__type {
+    font-size: 15px;
+  }
 
-.t-infos__status {
-  font-size: 10px;
-  padding: 2px 4px;
-  display: flex;
-  border: 1px solid #5357c1;
-  border-radius: 20px;
-}
+  &__visit {
+    font-size: 12px;
+  }
 
-.t-infos__footer {
-  margin-top: 7px;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  align-items: flex-end;
-  justify-content: space-between;
-  position: relative;
-}
+  &__status {
+    font-size: 10px;
+    padding: 2px 4px;
+    display: flex;
+    border: 1px solid #5357c1;
+    border-radius: 20px;
+  }
 
-.t-infos__fav {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 10px;
-}
+  &__footer {
+    margin-top: 7px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-between;
+    position: relative;
+  }
 
-.t-infos__heart-container {
-  padding: 5px;
-  border-radius: 100%;
-  background-color: #5357c1;
-}
-
-.t-infos__heart {
-  color: transparent;
-  fill: transparent;
-  height: 10px;
+  &__fav {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 10px;
+  }
 }
 </style>
