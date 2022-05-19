5e738f2610aaa970e75c50a7019017cfd1d99d28
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-05-19 15:04:34 +0200

    visit-page-02

--- apos/modules/place-page/index.js
+++ apos/modules/place-page/index.js
@@ -3,11 +3,9 @@ module.exports = {
   methods(self) {
     return {
       async visitPage(req) {
-        // console.log('req params slug', req.params.slug, 'req', req)
         const piece = await self.apos.place
           .find(req, { slug: req.params.slug })
           .toObject()
-        // console.log('piece: ', piece)
         self.setTemplate(req, 'visit')
         req.data.piece = piece
       },
@@ -16,7 +14,6 @@ module.exports = {
   extendMethods(self) {
     return {
       dispatchAll(_super) {
-        // console.log('Passing through dispatchAll')
         _super()
         self.dispatch('/:slug/visit', self.visitPage)
       },
