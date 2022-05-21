578bb839a66304edd5dbfc583db53a1f4816158d
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-05-22 00:15:09 +0200

    fix logs in terminal

--- apos/modules/@apostrophecms/util/index.js
+++ apos/modules/@apostrophecms/util/index.js
@@ -15,19 +15,19 @@ module.exports = {
   methods() {
     return {
       log(msg, customMsg) {
-        logger.info(msg, customMsg)
+        logger.info(`${msg} ${customMsg || ''}`)
       },
       info(msg, customMsg) {
-        logger.info(msg, customMsg)
+        logger.info(`${msg} ${customMsg || ''}`)
       },
       debug(msg, customMsg) {
-        logger.debug(msg, customMsg)
+        logger.debug(`${msg} ${customMsg || ''}`)
       },
       warn(msg, customMsg) {
-        logger.warn(msg, customMsg)
+        logger.warn(`${msg} ${customMsg || ''}`)
       },
       error(msg, customMsg) {
-        logger.error(msg, customMsg)
+        logger.error(`${msg} ${customMsg || ''}`)
       },
     }
   },
