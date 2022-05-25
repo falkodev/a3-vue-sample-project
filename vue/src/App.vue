a7498af87917e025b02d0906c012b4023e8137e5
ynck <ynck.chrl@protonmail.com>
2022-05-25 18:46:34 +0200

    visit-page-4

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,7 +4,7 @@ import {
   LTileLayer,
   LMarker,
   // LControlScale,
-  // LIcon,
+  LIcon,
   // LControlLayers,
   // LTooltip,
   // LPopup,
@@ -20,7 +20,7 @@ export default {
     LTileLayer,
     LMarker,
     // LControlScale,
-    // LIcon,
+    LIcon,
     // LControlLayers,
     // LTooltip,
     // LPopup,
@@ -32,10 +32,10 @@ export default {
   data() {
     return {
       allowZoom: false,
-      zoom: 14,
+      zoom: 15,
       userCoords: {
-        latitude: 43.6285,
-        longitude: 3.8694,
+        latitude: 43.6367,
+        longitude: 3.5866,
       },
     }
   },
@@ -52,13 +52,56 @@ export default {
     userLong() {
       return this.userCoords.longitude
     },
+    visitPath() {
+      return [
+        { type: 'start', lat: this.userLat, long: this.userLong },
+        {
+          type: 'point',
+          lat: this.userLat - 0.001,
+          long: this.userLong - 0.001,
+        },
+        {
+          type: 'lieu',
+          lat: this.userLat + 0.005,
+          long: this.userLong - 0.002,
+        },
+        {
+          type: 'point',
+          lat: this.userLat + 0.002,
+          long: this.userLong + 0.004,
+        },
+        {
+          type: 'lieu',
+          lat: this.userLat + 0.002,
+          long: this.userLong + 0.004,
+        },
+        { type: 'end', lat: this.userLat, long: this.userLong },
+      ]
+    },
+    lieux() {
+      return this.visitPath.filter((x) => x.type == 'lieu')
+    },
+    visitPoints() {
+      return this.visitPath.reduce((prev, curr) => {
+        prev.push([curr.lat, curr.long])
+        return prev
+      }, [])
+    },
   },
   methods: {
     setPosition(pos) {
-      let time = new Date().toUTCString()
-      console.log(pos.coords, time)
-      this.userCoords.latitude = pos.coords.latitude
-      this.userCoords.longitude = pos.coords.longitude
+      return new Promise((res) => {
+        res(pos)
+      })
+        .then((pos) => {
+          let time = new Date().toUTCString()
+          console.log(pos.coords, 'time :', time)
+          this.userCoords.latitude = pos.coords.latitude
+          this.userCoords.longitude = pos.coords.longitude
+        })
+        .catch((e) => {
+          console.log(e)
+        })
     },
     errorGettingPos(e) {
       console.log('error getting position ===>', e)
@@ -68,6 +111,11 @@ export default {
         navigator.geolocation.getCurrentPosition(
           this.setPosition,
           this.errorGettingPos,
+          {
+            timeout: 2000,
+            maximumAge: 0,
+            enableHighAccuracy: true,
+          },
         )
       } else {
         console.log(
@@ -80,6 +128,11 @@ export default {
         navigator.geolocation.watchPosition(
           this.setPosition,
           this.errorGettingPos,
+          {
+            timeout: 2000,
+            maximumAge: 0,
+            enableHighAccuracy: true,
+          },
         )
       } else {
         console.log(
@@ -88,11 +141,12 @@ export default {
       }
     },
   },
-  beforeMount() {},
+  beforeMount() {
+    this.watchUserPos()
+  },
   mounted() {
     console.log('this.domain ===>', this.domain)
-    this.watchUserPos()
-    setInterval(this.getUserPos, 1000)
+    console.log('visit points ===>', this.visitPoints)
   },
   updated() {
     this.getUserPos()
@@ -109,7 +163,7 @@ export default {
           draggable="false"
           :minZoom="zoom"
           :maxZoom="zoom"
-          v-model:zoom="zoom"
+          v-model="zoom"
           :center="[userLat, userLong]"
           bounceAtZoomLimits="true"
           zoomControl="false"
@@ -120,43 +174,92 @@ export default {
           ></l-tile-layer>
 
           <!-- User Marker -->
-          <l-marker
-            :lat-lng="[userLat, userLong]"
-            @moveend="console.log('moveend')"
-          >
+          <l-marker :lat-lng="[userLat, userLong]" @moveend="getUserPos">
+            <l-icon
+              iconUrl="/apos-frontend/default/modules/asset/icons/pin-user.svg"
+              iconSize="[120, 120]"
+            />
           </l-marker>
 
-          <l-marker :lat-lng="[userLat + 0.007, userLong - 0.002]"> </l-marker>
-          <l-marker :lat-lng="[userLat - 0.003, userLong - 0.001]"> </l-marker>
-          <l-marker :lat-lng="[userLat + 0.002, userLong + 0.004]"> </l-marker>
+          <l-marker
+            v-for="(lieu, lieuIndex) in lieux"
+            :key="'lieu-' + lieuIndex"
+            :lat-lng="[lieu.lat, lieu.long]"
+          ></l-marker>
 
-          <l-polyline
-            :lat-lngs="[
-              [userLat + 0.007, userLong - 0.002],
-              [userLat - 0.003, userLong - 0.001],
-              [userLat + 0.002, userLong + 0.004],
-              [userLat + 0.007, userLong - 0.002],
-            ]"
-            color="green"
-          ></l-polyline>
+          <l-polyline :lat-lngs="visitPoints" color="#e6004c"></l-polyline>
         </l-map>
       </div>
     </div>
 
     <div class="t-domain__content">
       <!-- <div class="t-domain__image"></div> -->
-      <div class="t-step__container">
-        <div
-          class="t-step__item"
-          v-for="(step, stepIndex) in domain.visit"
-          :key="stepIndex"
-        >
-          <p class="t-step__name">
-            <b>Etape {{ stepIndex + 1 }} :</b> {{ step.name }}
-          </p>
-          <span class="t-step__icon"></span>
+      <template v-for="(step, stepIndex) in domain.visit" :key="stepIndex">
+        <div class="t-step__container">
+          <div class="t-step__item">
+            <p class="t-step__name">
+              <b>Etape {{ stepIndex + 1 }} :</b> {{ step.name }}
+            </p>
+            <span class="t-step__icon"></span>
+          </div>
         </div>
-      </div>
+        <div class="t-media__container">
+          <div
+            class="t-media__item"
+            v-for="(media, mediaIndex) in step.media"
+            :key="mediaIndex"
+          >
+            <p class="t-media__info">{{ media.name }}</p>
+            <span :class="{ 't-media__download': media.downloadable }">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 112 112">
+                <circle
+                  cx="56"
+                  cy="56"
+                  r="48"
+                  fill="none"
+                  stroke="$color-purple"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="7.35"
+                />
+                <line
+                  x1="56"
+                  y1="24"
+                  x2="56"
+                  y2="88"
+                  fill="none"
+                  stroke="$color-purple"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="7.35"
+                />
+                <line
+                  x1="56"
+                  y1="88"
+                  x2="74"
+                  y2="64"
+                  fill="none"
+                  stroke="$color-purple"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="7.35"
+                />
+                <line
+                  x1="56"
+                  y1="88"
+                  x2="38"
+                  y2="64"
+                  fill="none"
+                  stroke="$color-purple"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="7.35"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
