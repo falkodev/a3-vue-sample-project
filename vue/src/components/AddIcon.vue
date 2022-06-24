db41cfdc05267fcd05196e9428d340334b610a2b
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-06-24 11:08:07 +0000

    Fix vite build prod

--- vue/src/components/AddIcon.vue
+++ vue/src/components/AddIcon.vue
@@ -6,9 +6,9 @@
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
       clip-rule="evenodd"
+      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+      fill-rule="evenodd"
     ></path>
   </svg>
 </template>
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '/assets/settings.scss';
+@import '../assets/settings.scss';
 
 .t-add-icon {
   color: $color-main;
