f34d02bd0ad1913d0367b30d1f56b9a5fa87eed5
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-06-26 13:00:45 +0000

    Merging Main into uat

---
+++ apos/modules/event-page/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extend: '@apostrophecms/piece-page-type',
+  options: {
+    label: 'apostrophe:event.label',
+  },
+
+  methods() {
+    return {
+      beforeIndex(req) {
+        req.notFound = true
+      },
+    }
+  },
+}
