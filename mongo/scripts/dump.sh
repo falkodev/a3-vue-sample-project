641d0578726d1065e062871ee959e5e8b661ad6d
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-07-01 18:06:50 +0200

    Release v.1.2.4

--- mongo/scripts/dump.sh
+++ mongo/scripts/dump.sh
@@ -3,4 +3,10 @@
 MONGO_INITDB_ROOT_USERNAME=$(grep MONGO_INITDB_ROOT_USERNAME .env | cut -d '=' -f2)
 MONGO_INITDB_ROOT_PASSWORD=$(grep MONGO_INITDB_ROOT_PASSWORD .env | cut -d '=' -f2)
 
-docker-compose exec -T vino-terr-mongo sh -c "mongodump --authenticationDatabase admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD" && docker cp vino-terr-mongo:/dump/vino-terr-larzac/ ./mongo/backup/dump-$(date +"%FT%H%M")
+NOW=$(date +"%FT%H%M")
+
+sudo mkdir -p ./mongo/backup && \
+docker-compose exec -T vino-terr-mongo sh -c "mongodump --authenticationDatabase admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD" && \
+sudo docker cp vino-terr-mongo:/dump/vino-terr-larzac/ ./mongo/backup/dump-$NOW && \
+sudo mkdir -p ../../mongo/data/backup && \
+sudo cp -r ./mongo/backup/dump-$NOW ../../mongo/data/backup
