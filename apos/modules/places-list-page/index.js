1f25596117e36024a6fac061a5a66b5f848af5a0
Anthony Tarlao <anthony.tarlao@gmail.com>
2022-11-24 10:34:30 +0100

    Update doc

--- apos/modules/places-list-page/index.js
+++ apos/modules/places-list-page/index.js
@@ -8,7 +8,6 @@ module.exports = {
 
   components(self) {
     return {
-
       async placesCategories(req) {
         const categories = config.get('categories')
         const criteria = {
@@ -17,30 +16,32 @@ module.exports = {
         const result = await Promise.all([
           self.apos.domain.find(req, criteria).limit(8).toArray(),
           ...categories.map((category) =>
-            self.apos.place.find(req, { placeType: category.value }).limit(8).toArray(),
+            self.apos.place
+              .find(req, { placeType: category.value })
+              .limit(8)
+              .toArray(),
           ),
         ])
         return { result }
       },
 
       async category(req) {
-        let parametersPlaces = req.absoluteUrl;
-        let result;
+        let parametersPlaces = req.absoluteUrl
+        let result
 
-        if (parametersPlaces == "/places-list?place=domain" ) {
+        if (parametersPlaces == '/places-list?place=domain') {
           result = await Promise.all([
-            self.apos.domain.find(req).toArray(),'domain',
+            self.apos.domain.find(req).toArray(),
+            'domain',
           ])
           return { result }
         } else {
-          if (parametersPlaces == "/places-list?place=wineStore" ) {
-            var placeUrl = "wineStore";
-          }
-          else if (parametersPlaces == "/places-list?place=wineBar" ) {
-            var placeUrl = "wineBar";
-          }
-          else if (parametersPlaces == "/places-list?place=poi" ) {
-            var placeUrl = "poi";
+          if (parametersPlaces == '/places-list?place=wineStore') {
+            var placeUrl = 'wineStore'
+          } else if (parametersPlaces == '/places-list?place=wineBar') {
+            var placeUrl = 'wineBar'
+          } else if (parametersPlaces == '/places-list?place=poi') {
+            var placeUrl = 'poi'
           }
           result = await Promise.all([
             self.apos.place.find(req, { placeType: placeUrl }).toArray(),
@@ -50,5 +51,5 @@ module.exports = {
         return { result }
       },
     }
-  }
+  },
 }
